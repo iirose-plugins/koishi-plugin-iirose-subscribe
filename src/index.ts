0bdf9b1ca3b262b7326cda788867b92b0e5ff8f2
shangxueink <1919892171@qq.com>
2025-11-09 15:20:01 +0800

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi';
+import { Context, h, Schema } from 'koishi';
 import { } from 'koishi-plugin-adapter-iirose';
 
 export const name = 'iirose-subscribe';
@@ -11,14 +11,20 @@ export const inject = {
 export const usage = `
 ---
 
-# 快速说明
-1. 在群内发送iirose.sub.on，将会订阅BOT的消息
-2. 在群内发送iirose.sub.off，将会取消订阅BOT的消息
-3. 订阅状态会在数据库中保存，不会因为重启而消失
-4. 本插件只会在iirose中生效
-5. adminList为管理员列表，需要填写用户的唯一标识(如:5b0fe8a3b1ff2)，只有管理员才能使用订阅功能
+## 插件效果
 
-# 注意事项
+频道消息 订阅转发。
+
+订阅一个频道，可以让你不在那个频道的时候，通过与机器人私聊 实现与订阅频道的交互。
+
+## 使用说明
+1. 在群内发送 iirose.sub.on ，将会订阅BOT的消息
+2. 在群内发送 iirose.sub.off ，将会取消订阅BOT的消息
+3. 订阅状态会在数据库 (iirose_subscribe表) 中保存，不会因为重启而消失
+4. 本插件只会在 iirose 中生效
+5. adminList 为管理员列表，需要填写用户的唯一标识(如: 5b0fe8a3b1ff2 )，只有管理员才能使用订阅功能
+
+## 注意事项
 开启订阅状态后，私聊bot发送内容，会被转发到群内
 开启订阅状态后，群内发送内容，会被转发到私聊bot
 关闭订阅状态后，不会再转发消息
@@ -33,7 +39,7 @@ export interface Config
 
 export const Config: Schema<Config> =
   Schema.object({
-    adminList: Schema.array(String).description('管理员uid列表')
+    adminList: Schema.array(String).description('管理员uid列表'),
   });
 
 declare module 'koishi' {
@@ -88,7 +94,7 @@ export function apply(ctx: Context, config: Config)
             status: true
           });
 
-          return `[IIROSE-Subscribe] 将 [*${session.username}*] 设置为BOT订阅状态`;
+          return `[IIROSE-Subscribe] 将 ${h.at(session.userId)} 设置为BOT订阅状态`;
         } else if (userData.length == 0)
         {
           ctx.database.create('iirose_subscribe', {
@@ -96,7 +102,7 @@ export function apply(ctx: Context, config: Config)
             status: true
           });
 
-          return `[IIROSE-Subscribe] 将 [*${session.username}*] 设置为BOT订阅状态`;
+          return `[IIROSE-Subscribe] 将 ${h.at(session.userId)} 设置为BOT订阅状态`;
         }
       });
 
@@ -125,7 +131,7 @@ export function apply(ctx: Context, config: Config)
             status: false
           });
 
-          return `[IIROSE-Subscribe] 将 [*${session.username}*] 设置为BOT取消订阅状态`;
+          return `[IIROSE-Subscribe] 将 ${h.at(session.userId)} 设置为BOT取消订阅状态`;
         } else if (userData.length == 0)
         {
           ctx.database.create('iirose_subscribe', {
@@ -133,7 +139,7 @@ export function apply(ctx: Context, config: Config)
             status: false
           });
 
-          return `[IIROSE-Subscribe] 将 [*${session.username}*] 设置为BOT取消订阅状态`;
+          return `[IIROSE-Subscribe] 将 ${h.at(session.userId)} 设置为BOT取消订阅状态`;
         }
       });
 
@@ -143,34 +149,36 @@ export function apply(ctx: Context, config: Config)
       {
         return;
       }
+      const userId = session.userId;
+      const userInput = session.content;
 
-      const userData = await ctx.database.get('iirose_subscribe', session.userId);
-
-      const msg = session.content;
-      const channelId = session.channelId;
-
-      if (channelId.startsWith('public'))
+      // 公屏消息处理
+      if (!session.isDirect)
       {
         const listTemp = await ctx.database.get('iirose_subscribe', { status: true });
         const list = listTemp.map(v => v.uid);
-        list.forEach(async v =>
+        for (const v of list)
         {
-          await session.bot.sendMessage(`private:${v}`, ` [*${session.username}*] ： ${msg}`);
-        });
-      } else if (channelId.startsWith('private'))
+          // 不把公屏消息发回给原始发送者
+          if (v === userId) continue;
+          await session.bot.sendMessage(`private:${v}`, `${h.at(userId)}：${userInput}`);
+        }
+      }
+      // 私聊消息处理
+      else
       {
+        const userData = await ctx.database.get('iirose_subscribe', { uid: userId });
+        // 检查用户是否订阅
         if (userData.length <= 0 || !userData[0].status)
         {
           return;
         }
 
-        await session.bot.sendMessage('public', ` [*${session.username}*] ： ${msg}`);
-        const listTemp = await ctx.database.get('iirose_subscribe', { status: true });
-        const list = listTemp.map(v => v.uid);
-        list.forEach(async v =>
-        {
-          await session.bot.sendMessage(`private:${v}`, ` [*${session.username}*] ： ${msg}`);
-        });
+        const botroom = session.bot.config.roomId;
+        if (!botroom) return;
+
+        // 将私聊消息转发到指定群聊
+        await session.bot.sendMessage(`public:${botroom}`, `${h.at(userId)}：${userInput}`);
       }
     });
 
